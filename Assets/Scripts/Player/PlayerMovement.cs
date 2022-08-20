72f45334d8ca76fc47dd26a9175f4349aa6ba3c7
Andrés Julián Álvarez Espinosa <andresjales@gmail.com>
2022-08-20 14:52:29 -0500

    Joystick Movement Ready

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController characterController;
+    [SerializeField] Joystick joystick;
+
+    public float speed;
+    public float smoothTurnTime;
+
+    private float smoothTurnVelocity;
+
+    private void Update()
+    {
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurnVelocity, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            characterController.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
