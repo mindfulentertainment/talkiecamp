bb62a2ff1375f343854b7ca628869c2c5b615b58
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-11-15 20:03:12 -0500

    fire fix

--- Assets/Scripts/FireSystem/Extinguisher.cs
+++ Assets/Scripts/FireSystem/Extinguisher.cs
@@ -6,49 +6,81 @@ using System.Data;
 
 public class Extinguisher : MonoBehaviourPun
 {
-    [SerializeField] private LayerMask fireLayer;
     [SerializeField] private float amountToExtinguish;
     [SerializeField] private GameObject fireExtinguisher;
     private FireNetwork fire;
+    string myRole;
 
+  
+    private void Start()
+    {
+        myRole =this.gameObject.GetComponent<PlayerController>().role;
+
+    }
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-
-
-            Ray touchRay = TouchManager.GenerateTouchRay(Input.mousePosition);
-            RaycastHit hit;
-
-            
-            if (Physics.Raycast(touchRay.origin, touchRay.direction, out hit, 100, fireLayer))
+        
+            if (Input.GetMouseButton(0))
             {
-                fire = hit.collider.GetComponent<FireNetwork>();
+                
 
-                if ((Vector3.Distance(hit.point, transform.position) <= 5) &&fire!=null&&fire.GetComponent<ActivationManager>().onFire )
+
+                Ray touchRay = TouchManager.GenerateTouchRay(Input.mousePosition);
+                RaycastHit hit;
+
+
+                if (Physics.Raycast(touchRay.origin, touchRay.direction, out hit, 100))
                 {
-                    if (LayerMask.LayerToName(hit.collider.gameObject.layer) == "Watchmen")
+                    if (photonView.IsMine)
                     {
-                        photonView.RPC("ShowTool", RpcTarget.AllViaServer, true);
-                        fire.TryExtinguish(amountToExtinguish * Time.deltaTime);
-                    }
+                        if (LayerMask.LayerToName(hit.collider.gameObject.layer) == "Watchmen")
+                        {
+
+                                var burn = hit.collider.GetComponentInParent<Burn>();
+
+                        if (burn!= null){
+                            fire = burn.GetComponentInChildren<FireNetwork>();
+                        }
+
+                            if ((Vector3.Distance(hit.point, transform.position) <= 5) && fire != null && fire.GetComponent<ActivationManager>().onFire)
+                            {
 
                         
+
+                                    photonView.RPC("ShowTool", RpcTarget.AllViaServer, true);
+                                    fire.TryExtinguish(amountToExtinguish * Time.deltaTime);
+                                fire = null;
+
+
+                            }
+                        }
+                    }
+                  
+
+
+                }
+            }
+            else
+            {
+                if (photonView.IsMine)
+                {
+                    UIController.instance.slider.gameObject.SetActive(false);
+
+                }
+        }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (photonView.IsMine)
+                {
+                    photonView.RPC("ShowTool", RpcTarget.AllViaServer, false);
+
+
                 }
 
-              
-            }
         }
-        else
-        {
-            UIController.instance.slider.gameObject.SetActive(false);
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            photonView.RPC("ShowTool", RpcTarget.AllViaServer, false);
-
-        }
+        
+        
     }
     [PunRPC]
     void ShowTool(bool state)
--- Assets/Scripts/Ricardo/Gather Stuff/GatheringManager.cs
+++ Assets/Scripts/Ricardo/Gather Stuff/GatheringManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class GatheringManager : MonoBehaviour
 {
